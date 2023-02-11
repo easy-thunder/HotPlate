c7cc1b1ae95ce31a75017e6cbe6c2fd7e226372b
Ebyan Jamal <Ebyanjamal6@gmail.com>
2023-02-11 08:07:23 -0700

    pulling

--- @latest/src/noUserInfo/Menu.jsx
+++ @latest/src/noUserInfo/Menu.jsx
@@ -6,16 +6,16 @@ function Menu({menuItem}){
     console.log("hi")
     console.log(menuItem.name)
     return(
-    <div className="  ">
-        <h3>{menuItem.name}</h3>
+    <div>
+        <h3 className="text-white">{menuItem.name}</h3>
 
         <h3> {menuItem.description} </h3>
 
         <h3>{menuItem.price}</h3>
 
-        <button  className='btn capitalize w-full lg:max-w-[240px] '>add too order</button>
+        <button className='btn capitalize w-full lg:max-w-[240px] '>add too order</button>
 
-    </ div>)
+    </div>)
 }
 
 export default Menu
